5179c93c8f50ceffe070d7807f6094b4d6a68683
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2023-10-03 17:04:06 +0200

    Create 01.beginnig.R

---
+++ 01.beginnig.R
@@ -0,0 +1,16 @@
+# here I can write anything you want, and nothing can happen. It is useful to make viewers to undersant what you are doing
+
+# R as a calculaotor 
+2+3
+
+# assing to an object
+zima <- 2+3
+zima
+
+duccio <- 5+3
+duccio
+
+final <- zima*duccio
+final
+
+final^2
