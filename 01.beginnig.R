c0b9b6f2ae7a4371bcecba83263360f8e496fbf5
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2023-10-03 17:52:22 +0200

    Update 01.beginnig.R

--- 01.beginnig.R
+++ 01.beginnig.R
@@ -14,3 +14,25 @@ final <- zima*duccio
 final
 
 final^2
+
+# array, a series of numbers put together 
+sophie <- c(10,20,30,50,70) #microplastics, numbers are called arguments and functions have parentheses
+
+paula <- c(100,500,600,1000,2000) #people
+
+plot(paula , sophie)
+
+plot(paula , sophie, xlab="number of people", ylab="microplastics")
+
+people <- paula
+microplastics <- sophie
+
+plot(people, microplastics)
+
+#http://www.sthda.com/sthda/RDoc/images/points-symbols.png
+
+plot(people, microplastics, pch=19)
+
+plot(people, microplastics, pch=19, cex=2)
+
+plot(people, microplastics, pch=19, cex=2, col="blue")
