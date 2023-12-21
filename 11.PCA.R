fea48c343c99de95d45f7a1b987d8b3fd7f6a2c6
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2023-12-21 17:48:44 +0100

    Create 11.PCA.R

---
+++ 11.PCA.R
@@ -0,0 +1,38 @@
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+pairs(sent) #sentinel 2 and 3 (red and green) are really correlated to each other (0.98), the NIR is less correlated so it is not adding informations in fact we should remove it
+
+#perform PCA on sent
+sentpc <- im.pca(sent)
+
+pc1 <- sentpc$PC1
+viridisc <- colorRampPalette(viridis(7))(255)
+plot(pc1, col=viridisc)
+
+#calculating standard deviation on top of pc1
+pc1sd3<-focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd3, col=viridisc)
+#we calculated on a single layer
+
+pc1sd7<-focal(pc1, matrix(1/49, 7, 7), fun=sd)
+plot(pc1sd7, col=viridisc)
+
+par(mfrow=c(2,3))
+im.plotRGB(sent, 2, 1, 3)
+#sd from the vaiability script:
+plot(sd3, col=viridisc)
+plot(sd7, col=viridisc)
+plot(pc1, col=viridisc)
+plot(pc1sd3, col=viridisc)
+plot(pc1sd7, col=viridisc)
+
+#stack all the standard deviation layers
+sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
+names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7") 
+plot(sdstack, col=viridisc)
+
