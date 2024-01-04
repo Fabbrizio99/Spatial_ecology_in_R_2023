88bcacf5b9055ad93944a7eec7620044000f4ca6
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2024-01-04 18:50:55 +0100

    Update 03.communities.multivar.R

--- 03.communities.multivar.R
+++ 03.communities.multivar.R
@@ -1,11 +1,17 @@
+install.packages("vegan")
+library(vegan)
+install.packages("overlap")
+library(overlap)
+
+# we need to recall the data from the dataset with the function data()
 data(dune)
 dune
-head(dune) #ci dà solamente le prime 6 righe di dati
-tail(dune) #ultime 6 righe
+head(dune) # it gives us only the first 6 rows of data
+tail(dune) # last 6 rows
 
-ord <- decorana(dune) #Performs detrended correspondence analysis
+ord <- decorana(dune) #Performs detrended correspondence analysis, to see how species are correlated together in a plot
 
-#meausure the lenght
+#meausures of the axis lenght
 ldc1 = 3.7004
 ldc2 = 3.1166
 ldc3 = 1.30055
@@ -13,6 +19,7 @@ ldc4 = 1.47888
 
 total = ldc1+ldc2+ldc3+ldc4
 
+#calculate thee percentage of each axes lenght to the total 
 pldc1= ldc1 * 100 / total
 pldc2= ldc2 * 100 / total
 pldc3= ldc3 * 100 / total
@@ -20,11 +27,14 @@ pldc4= ldc4 * 100 / total
 
 pldc1  
 pldc2
+plcd3
+plcd4
 
 total
 
-pldc1 + pldc2
+pldc1 + pldc2 # togethere tey are more than 70%
 
+#final plot, we can see how different specis are related to each other
 plot(ord)
 
 
