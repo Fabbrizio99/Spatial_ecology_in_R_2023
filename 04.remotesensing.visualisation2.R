f0a57d31bc17495e5dea889a08b383c2d6ca42f1
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2024-02-03 12:12:16 +0100

    Update 04.remotesensing.visualisation2.R

--- 04.remotesensing.visualisation2.R
+++ 04.remotesensing.visualisation2.R
@@ -8,7 +8,10 @@ devtools::install_github("ducciorocchini/imageRy")
 
 library(imageRy)
 
+# to see all the data in the package, all functions from imageRy start with im."something"
 im.list()
 
+#how to import data from the sentinel satellite from ESA
+#we ewill use images with the same spacial resolution (10 meters), with 4 diffrent bands 
 b2 <- im.import("sentinel.dolomites.b2.tif")
 b2
