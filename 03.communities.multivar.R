6f9d4ffeded0256e35b36062a0e142f958cb4956
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2025-04-29 15:58:12 +0200

    Update 03.communities.multivar.R

--- 03.communities.multivar.R
+++ 03.communities.multivar.R
@@ -1,3 +1,5 @@
+#we will use vegan package for our multivariate analisys for community ecology. Vegan means vegetation-analysis
+#Overlap package is perfect to see how species are interacting over time
 install.packages("vegan")
 library(vegan)
 install.packages("overlap")
@@ -9,7 +11,11 @@ dune
 head(dune) # it gives us only the first 6 rows of data
 tail(dune) # last 6 rows
 
-ord <- decorana(dune) #Performs detrended correspondence analysis, to see how species are correlated together in a plot
+#we can use the "detrended correspondence analysis" to see how species are correlated between each other in a plot. 
+#this also helps us to reshape the data in a simpler manner
+#to do this, we use the function decorana ()
+ord <- decorana(dune) 
+ord
 
 #meausures of the axis lenght
 ldc1 = 3.7004
@@ -19,7 +25,7 @@ ldc4 = 1.47888
 
 total = ldc1+ldc2+ldc3+ldc4
 
-#calculate thee percentage of each axes lenght to the total 
+#calculate the percentage of each axe length to the total 
 pldc1= ldc1 * 100 / total
 pldc2= ldc2 * 100 / total
 pldc3= ldc3 * 100 / total
@@ -32,7 +38,7 @@ pldc4
 
 total
 
-pldc1 + pldc2 # togethere tey are more than 70%
+pldc1 + pldc2 # together tey are more than 70%
 
 #final plot, we can see how different specis are related to each other
 plot(ord)
