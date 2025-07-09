3d9b5a37e328fa55f018aa897a20309f57045d4c
Fabbrizio99 <146854132+Fabbrizio99@users.noreply.github.com>
2025-07-09 13:10:03 +0200

    Update 10.variability.R

--- 10.variability.R
+++ 10.variability.R
@@ -1,5 +1,5 @@
-#the higher the variabilit, the higher the nubmber of species
-#measurement from RS based variability
+#the higher the variability, the higher the number of species
+#measurement from RS-based variability
 
 library(imageRy)
 library(terra)
@@ -17,18 +17,18 @@ im.plotRGB(sent, r=1, g=2, b=3) #vegetation is red here, since the NIR is in the
 im.plotRGB(sent, r=2, g=1, b=3) #vegetation is green since NIR is in the second band 
 
 nir <- sent [[1]]
-plot(nir) #green part is vegetatio, orange is bare soil, 
+plot(nir) #green part is vegetation, orange is bare soil, 
 
 
-#moving window--> it calculates the standard deviations of the centrale pixel from a nine selection(3x3) and it is moving until it measure the whole picture, it puts the value in the
+#moving window--> it calculates the standard deviations of the central pixel from a nine selection(3x3) and it is moving until it measures the whole picture, it puts the value in the
 #central pixel each time 
-#focal, we use the band nir, matrix is describing the dimension of the moving window that here is a 3x3, we descfribe the function we want to use
+#focal, we use the band nir, matrix is describing the dimension of the moving window that here is a 3x3, we describe the function we want to use
 sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
 plot(sd3)
-# we are changing  colors, and the legend with the package viridis (255 is the umbeer of colors that are in the package viridis)
+# we are changing colors, and the legend with the package viridis (255 is the umbeer of colors that are in the package viridis)
 viridisc<-colorRampPalette(viridis(7))(255)
 plot(sd3, col=viridisc)
-#the area in which there is more variability is north-west
+# The area in which there is more variability is north-west
 #calculate the variability in a 7x7 pixels moving window
 sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
 plot(sd7)
